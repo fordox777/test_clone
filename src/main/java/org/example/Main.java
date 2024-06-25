b7158359c3d024fda0276e3392251f2ad0ba49ee
Fordox Fordox <fordox777@gmail.com>
2024-06-25 11:30:16 +0200

    4-commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,6 +14,7 @@ public class Main {
             System.out.println("i = " + i);
             System.out.println("Hello");
             System.out.println("Hello1");
+            System.out.println("Hello2");
         }
     }
 }
